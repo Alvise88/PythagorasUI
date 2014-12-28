8df9deafc864717c265229be93553c2616db1bbd
alvise88 <vitalvise@gmail.com>
2014-12-28 12:03:53 +0000

    Testing 1

---
+++ core/src/main/directives/agoraDatatable.js
@@ -0,0 +1,14 @@
+/**
+ * Created by alvise on 28/12/14.
+ */
+
+
+(function(){
+    angular.module('agora.widgets', []).factory('datatableFactory', function(){
+        datatableFactory = {};
+
+        datatableFactory.prova = function(){
+            return "prova";
+        };
+    });
+})();
---
+++ core/src/test/js/agoraDatatableTest.js
@@ -0,0 +1,11 @@
+/**
+ * Created by alvise on 28/12/14.
+ */
+describe('angularjs homepage', function() {
+    it('should have a title', function() {
+        //browser.get('http://juliemr.github.io/protractor-demo/');
+        browser.get('PythagorasUI/core/src/test/html/agoraDatatableTest.html');
+
+        expect(browser.getTitle()).toEqual('agoraDatatable');
+    });
+});
